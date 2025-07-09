7afb72b5a8a3231fdd355d389d27b5f63e8514f2
xiaohai1314-sys <1083328569@qq.com>
2025-07-09 22:41:53 +0800

    Update guanying.js

--- js/guanying.js
+++ js/guanying.js
@@ -1,15 +1,14 @@
-
 const cheerio = createCheerio()
 const UA = 'Mozilla/5.0 (AppleTV; CPU OS 18_2 like Mac OS X) AppleWebKit/604.1.14 (KHTML, like Gecko)'
 
 const appConfig = {
-  ver: 2,
-  title: '观影网 (新版)',
+  ver: 3,
+  title: '观影网（最终版）',
   site: 'https://www.gying.org',
   tabs: [
-    { name: '电影', ext: { id: '/mv/------' } },
-    { name: '剧集', ext: { id: '/tv/------' } },
-    { name: '动漫', ext: { id: '/ac/------' } },
+    { name: '电影', ext: { id: '/mv/' } },
+    { name: '剧集', ext: { id: '/tv/' } },
+    { name: '动漫', ext: { id: '/ac/' } },
   ],
 }
 
@@ -20,29 +19,22 @@ async function getConfig() {
 async function getCards(ext) {
   ext = argsify(ext)
   let cards = []
-  let { page = 1, id } = ext
+  const { page = 1, id } = ext
   const url = appConfig.site + id + page
-
-  const { data } = await $fetch.get(url, {
-    headers: { "User-Agent": UA },
-  })
-
+  const { data } = await $fetch.get(url, { headers: { "User-Agent": UA } })
   const $ = cheerio.load(data)
 
-  $('.swiper-slide a').each((i, el) => {
-    const name = $(el).attr('title') || '未知'
+  $('.swiper-wrapper .swiper-slide a').each((_, el) => {
     const href = $(el).attr('href')
+    const title = $(el).attr('title')
     const pic = $(el).find('img').attr('data-src')
-
-    if (href && name) {
+    if (href && title && pic) {
       cards.push({
         vod_id: href,
-        vod_name: name,
-        vod_pic: pic?.startsWith('http') ? pic : `${pic}`,
+        vod_name: title,
+        vod_pic: pic,
         vod_remarks: '',
-        ext: {
-          url: appConfig.site + href,
-        },
+        ext: { url: appConfig.site + href }
       })
     }
   })
@@ -52,38 +44,20 @@ async function getCards(ext) {
 
 async function getTracks(ext) {
   ext = argsify(ext)
-  const url = ext.url
-  const { data } = await $fetch.get(url, {
-    headers: { "User-Agent": UA },
-  })
-
+  const { data } = await $fetch.get(ext.url, { headers: { "User-Agent": UA } })
   const $ = cheerio.load(data)
   let tracks = []
-
-  $('#sBox .player-all a').each((i, el) => {
+  $('#sBox .player-all a').each((_, el) => {
     const name = $(el).text().trim()
     const href = $(el).attr('href')
     if (href && name) {
-      tracks.push({
-        name,
-        url: appConfig.site + href,
-        ext: { url: appConfig.site + href }
-      })
+      tracks.push({ name, url: appConfig.site + href, ext: { url: appConfig.site + href } })
     }
   })
-
   if (tracks.length === 0) {
-    $utils.toastError("未找到播放资源")
+    $utils.toastError("未找到播放线路")
   }
-
-  return jsonify({
-    list: [
-      {
-        title: '播放线路',
-        tracks,
-      },
-    ],
-  })
+  return jsonify([{ title: '线路列表', tracks }])
 }
 
 async function getPlayinfo(ext) {
@@ -94,33 +68,23 @@ async function getPlayinfo(ext) {
 async function search(ext) {
   ext = argsify(ext)
   const text = encodeURIComponent(ext.text)
-  const page = ext.page || 1
-  const url = `${appConfig.site}/s/1---${page}/${text}`
-
-  const { data } = await $fetch.get(url, {
-    headers: { "User-Agent": UA },
-  })
-
+  const url = `${appConfig.site}/s/1---${(ext.page || 1)}/${text}`
+  const { data } = await $fetch.get(url, { headers: { "User-Agent": UA } })
   const $ = cheerio.load(data)
   let cards = []
-
-  $('.swiper-slide a').each((i, el) => {
-    const name = $(el).attr('title') || '未知'
+  $('.swiper-wrapper .swiper-slide a').each((_, el) => {
     const href = $(el).attr('href')
+    const title = $(el).attr('title')
     const pic = $(el).find('img').attr('data-src')
-
-    if (href && name) {
+    if (href && title && pic) {
       cards.push({
         vod_id: href,
-        vod_name: name,
-        vod_pic: pic?.startsWith('http') ? pic : `${pic}`,
+        vod_name: title,
+        vod_pic: pic,
         vod_remarks: '',
-        ext: {
-          url: appConfig.site + href,
-        },
+        ext: { url: appConfig.site + href }
       })
     }
   })
-
   return jsonify({ list: cards })
 }
