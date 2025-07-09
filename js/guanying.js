eed86b20f6d6646bd951161588f8b46d0dfa76f3
xiaohai1314-sys <1083328569@qq.com>
2025-07-09 22:00:48 +0800

    Update guanying.js

--- js/guanying.js
+++ js/guanying.js
@@ -1,33 +1,17 @@
-//来自群友'Qi Qi'
 const cheerio = createCheerio()
-const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/604.1.14 (KHTML, like Gecko)'
+const UA = 'Mozilla/5.0 (AppleTV; CPU OS 18_2 like Mac OS X) AppleWebKit/604.1.14 (KHTML, like Gecko)'
 
 const appConfig = {
 	ver: 1,
 	title: '观影网',
 	site: 'https://www.gying.org',
 	tabs: [
-		{
-			name: '电影',
-			ext: {
-				id: '/mv/------',
-			},
-		},
-		{
-			name: '剧集',
-			ext: {
-				id: '/tv/------',
-			},
-		},
-		{
-			name: '动漫',
-			ext: {
-				id: '/ac/------',
-			},
-		}
-		
+		{ name: '电影', ext: { id: '/mv/------' } },
+		{ name: '剧集', ext: { id: '/tv/------' } },
+		{ name: '动漫', ext: { id: '/ac/------' } },
 	],
 }
+
 async function getConfig() {
 	return jsonify(appConfig)
 }
@@ -36,83 +20,70 @@ async function getCards(ext) {
 	ext = argsify(ext)
 	let cards = []
 	let { page = 1, id } = ext
-	const url =appConfig.site + id + page
-	const { data } = await $fetch.get(url, {
-    headers: {
-		"User-Agent": UA,
-  	  },
-});
+	const url = appConfig.site + id + page
+	const { data } = await $fetch.get(url, { headers: { "User-Agent": UA } })
+
 	const $ = cheerio.load(data)
-	const t1 = $('p.error').text()
-	  if ($('p.error').length > 0) { 
-		$utils.openSafari(appConfig.site, UA);
-	  }
-	
+
+	if ($('p.error').length > 0) {
+		$utils.toastError("页面加载失败，可能被拦截")
+		return jsonify({ list: [] })
+	}
+
 	const scriptContent = $('script').filter((_, script) => {
-			return $(script).html().includes('_obj.header');
-		}).html();
+		return $(script).html()?.includes('_obj.header')
+	}).html()
 
-		const jsonStart = scriptContent.indexOf('{');
-		const jsonEnd = scriptContent.lastIndexOf('}') + 1;
-		const jsonString = scriptContent.slice(jsonStart, jsonEnd);
+	if (!scriptContent) {
+		$utils.toastError("未找到视频数据脚本")
+		return jsonify({ list: [] })
+	}
 
-		const inlistMatch = jsonString.match(/_obj\.inlist=({.*});/);
-		if (!inlistMatch) {
-		$utils.toastError("未找到 _obj.inlist 数据");
-		} else {
-	
-		const inlistData = JSON.parse(inlistMatch[1]);
-	
-		inlistData["i"].forEach((item,index)=>{
-	  
-	  	cards.push({
-				  vod_id: item,
-				  vod_name: inlistData["t"][index],
-				  vod_pic: `https://s.tutu.pm/img/${inlistData["ty"]}/${item}.webp`,
-				  vod_remarks: inlistData["g"][index], 
-				  ext: {
-					  url: `https://www.gyg.la/res/downurl/${inlistData["ty"]}/${item}`,
-				  },
-			  })
-	})	
-}
-		return jsonify({
-		list: cards,
+	const jsonMatch = scriptContent.match(/_obj\.inlist=({.*});/)
+	if (!jsonMatch) {
+		$utils.toastError("未找到 _obj.inlist 数据")
+		return jsonify({ list: [] })
+	}
+
+	const inlistData = JSON.parse(jsonMatch[1])
+	inlistData["i"].forEach((item, index) => {
+		cards.push({
+			vod_id: item,
+			vod_name: inlistData["t"][index],
+			vod_pic: `https://s.tutu.pm/img/${inlistData["ty"]}/${item}.webp`,
+			vod_remarks: inlistData["g"][index],
+			ext: {
+				url: `https://www.gyg.la/res/downurl/${inlistData["ty"]}/${item}`,
+			},
+		})
 	})
+
+	return jsonify({ list: cards })
 }
 
 async function getTracks(ext) {
-	
 	ext = argsify(ext)
-    let tracks = []
 	let url = ext.url
-	const { data } = await $fetch.get(url, {
-		headers: {
-			'User-Agent': UA,
-		},
-	})
-	 const respstr =JSON.parse(data)
+	const { data } = await $fetch.get(url, { headers: { "User-Agent": UA } })
+	const respstr = JSON.parse(data)
 
-	 if(respstr.hasOwnProperty('panlist')){
+	let tracks = []
 
-     respstr.panlist.url.forEach((item, index) => {
+	if (respstr.hasOwnProperty('panlist')) {
+		respstr.panlist.url.forEach(item => {
 			tracks.push({
-				name:'网盘',
+				name: '网盘',
 				pan: item,
-				ext: {
-					url: '',
-				},
+				ext: { url: '' },
 			})
 		})
-   }else if(respstr.hasOwnProperty('file')){
+	} else if (respstr.hasOwnProperty('file')) {
+		$utils.toastError('网盘验证掉签')
+	} else {
+		$utils.toastError('没有网盘资源')
+	}
 
-   $utils.toastError('网盘验证掉签')
-   }else{
-
-	$utils.toastError('没有网盘资源');
-	
-}
-   return jsonify({
+	return jsonify({
 		list: [
 			{
 				title: '默认分组',
@@ -124,46 +95,35 @@ async function getTracks(ext) {
 
 async function getPlayinfo(ext) {
 	ext = argsify(ext)
-	const url = ext.url
-   	  
 	return jsonify({ urls: [ext.url] })
 }
 
 async function search(ext) {
 	ext = argsify(ext)
-	
-
 	let text = encodeURIComponent(ext.text)
 	let page = ext.page || 1
 	let url = `${appConfig.site}/s/1---${page}/${text}`
-
-	const { data } = await $fetch.get(url, {
-	   headers: {
-	"User-Agent": UA,
-    	},
-	})
+	const { data } = await $fetch.get(url, { headers: { "User-Agent": UA } })
 
 	const $ = cheerio.load(data)
-let cards = []
-   $('.v5d').each((index, element) => {
-   const name = $(element).find('b').text().trim() || 'N/A';
-   const imgUrl = $(element).find('picture source[data-srcset]').attr('data-srcset') || 'N/A';
-  
-  	const additionalInfo = $(element).find('p').text().trim() || 'N/A';
+	let cards = []
 
-  	const pathMatch =  $(element).find('a').attr('href') || 'N/A'
+	$('.v5d').each((_, element) => {
+		const name = $(element).find('b').text().trim() || 'N/A'
+		const imgUrl = $(element).find('picture source[data-srcset]').attr('data-srcset') || 'N/A'
+		const additionalInfo = $(element).find('p').text().trim() || 'N/A'
+		const pathMatch = $(element).find('a').attr('href') || 'N/A'
+
 		cards.push({
 			vod_id: pathMatch,
 			vod_name: name,
 			vod_pic: imgUrl,
 			vod_remarks: additionalInfo,
-
 			ext: {
 				url: `${appConfig.site}/res/downurl${pathMatch}`,
 			},
 		})
-});
-	return jsonify({
-		list: cards,
 	})
+
+	return jsonify({ list: cards })
 }
