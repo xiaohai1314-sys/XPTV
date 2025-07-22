ce72f466b8a707d66e717adc72bd9b8cf7184ff3
xiaohai1314-sys <1083328569@qq.com>
2025-07-22 16:20:52 +0800

    Update guanying.js

--- js/guanying.js
+++ js/guanying.js
@@ -7,7 +7,7 @@
  */
 
 // ==================== 配置区 ====================
-const API_BASE_URL = 'http://192.168.1.6:3001/api'; // 【重要】请再次确认这是您电脑的正确IP地址
+const API_BASE_URL = 'http://192.168.10.111:3001/api'; // 【重要】请再次确认这是您电脑的正确IP地址
 const UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64  ) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
 
 // ==================== 工具函数、配置、缓存区 ====================
