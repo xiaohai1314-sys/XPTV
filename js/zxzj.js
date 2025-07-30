59b30e39b2c698626f0bb29ee09117291b272514
xiaohai1314-sys <1083328569@qq.com>
2025-07-30 13:47:17 +0800

    Update zxzj.js

--- js/zxzj.js
+++ js/zxzj.js
@@ -1,46 +1,54 @@
 const cheerio = createCheerio()
-const UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
 
+const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/604.1.14 (KHTML, like Gecko)'
 const appConfig = {
-  ver: 1,
-  title: "在线之家",
-  site: "https://www.zxzja.com",
-  tabs: [{
-    name: '首页',
-    ext: {
-      id: 0,
-    },
-  }, {
-    name: '电影',
-    ext: {
-      id: 1,
-    },
-  }, {
-    name: '美剧',
-    ext: {
-      id: 2,
-    },
-  }, {
-    name: '韩剧',
-    ext: {
-      id: 3,
-    },
-  }, {
-    name: '日剧',
-    ext: {
-      id: 4,
-    },
-  }, {
-    name: '泰剧',
-    ext: {
-      id: 5,
-    },
-  }, {
-    name: '动漫',
-    ext: {
-      id: 6,
-    },
-  }]
+    ver: 1,
+    title: 'SupJav',
+    site: 'https://supjav.com/zh',
+    tabs: [
+        {
+            name: '热门',
+            ext: {
+                id: 'popular',
+            },
+            ui: 1,
+        },
+        {
+            name: '有码',
+            ext: {
+                id: 'category/censored-jav',
+            },
+            ui: 1,
+        },
+        {
+            name: '无码',
+            ext: {
+                id: 'category/uncensored-jav',
+            },
+            ui: 1,
+        },
+        {
+            name: '素人',
+            ext: {
+                id: 'category/amateur',
+            },
+            ui: 1,
+        },
+        {
+            name: '中文字幕',
+            ext: {
+                id: 'category/chinese-subtitles',
+            },
+            ui: 1,
+        },
+        {
+            name: '无码破解',
+            ext: {
+                id: 'category/reducing-mosaic',
+            },
+            ui: 1,
+        },
+    ],
 }
 
 async function getConfig() {
@@ -48,191 +56,203 @@ async function getConfig() {
 }
 
 async function getCards(ext) {
-  ext = argsify(ext)
-  var cards = []
-  let id = ext.id
-  let page = ext.page || 1
+    ext = argsify(ext)
+    let cards = []
+    let { page = 1, id } = ext
 
-  var url = "https://www.zxzja.com/"
-  if (id == 0 && page > 1) {
-      return jsonify({
-        list: cards,
-    });
-  }
+    const url = appConfig.site + `/${id}/page/${page}`
 
-  if (id > 0) {
-    url = `https://www.zxzja.com/list/${id}.html`
-    if (page > 1) {
-      url = `https://www.zxzja.com/list/${id}-${page}.html`
-    }
-  }
-
-  const { data } = await $fetch.get(url, {
-      headers: {
-        'Referer': 'https://www.zxzja.com/',
-        'User-Agent': UA,
-      }
-  });
-  
-  const $ = cheerio.load(data)
-  $('a.lazyload').each((_, element) => {
-    const href = $(element).attr('href')
-    const title = $(element).attr('title')
-    const cover = $(element).attr('data-original')
-    const subTitle = $(element).find('.text-right').text()
-
-    if (href.startsWith('/detail/')) {
-      cards.push({
-        vod_id: href.replace(/.*?\/detail\/(.*).html/g, '$1'),
-        vod_name: title,
-        vod_pic: cover,
-        vod_remarks: subTitle,
-        ext: {
-          url: `${appConfig.site}${href}`,
+    const { data } = await $fetch.get(url, {
+        headers: {
+            'User-Agent': UA,
         },
-      });
-    }
-  });
+    })
 
-  return jsonify({
-      list: cards,
-  });
+    const $ = cheerio.load(data)
+    const t1 = $('title').text()
+      if (t1 === 'Just a moment...') {
+        $utils.openSafari(appConfig.site, UA)
+      }
+
+    const videos = $('.post')
+    videos.each((_, e) => {
+        const href = $(e).find('a').attr('href')
+        const title = $(e).find('a').attr('title')
+        const cover = $(e).find('a img').attr('src').replace('!320x216.jpg', '')
+        const remarks = $(e).find('.con .meta .date').text()
+
+        cards.push({
+            vod_id: href,
+            vod_name: title,
+            vod_pic: cover,
+            vod_remarks: '',
+            ext: {
+                url: href,
+            },
+        })
+    })
+
+    return jsonify({
+        list: cards,
+    })
 }
 
 async function getTracks(ext) {
     ext = argsify(ext)
-    var tracks = []
+    let tracks = []
     let url = ext.url
 
-    // 发送请求
     const { data } = await $fetch.get(url, {
         headers: {
-          'Referer': 'https://www.zxzja.com/',
-          'User-Agent': UA,
-        }
-    });
-    
+            'User-Agent': UA,
+        },
+    })
+
     const $ = cheerio.load(data)
-    
-    // 解析数据，例如提取标题
-    $('.stui-content__playlist > li > a').each((_, each) => {
-        const href = $(each).attr('href')
-        const name = $(each).text()
-        if (href && name && name !== "合集") {
-          tracks.push({
-              name,
-              pan: "",
-              ext: {
-                'url': `${appConfig.site}${href}`,
-              }
-          });
-        }
-    });
-    
-    return jsonify({ list: [{
-        title: "默认分组",
-        tracks,
-    }]})
+    const btns = $('a.btn-server')
+    btns.each((_, e) => {
+        const name = $(e).text()
+        const data_link = $(e).attr('data-link')
+        tracks.push({
+            name,
+            pan: '',
+            ext: {
+                data: data_link,
+                name,
+            },
+        })
+    })
+
+    return jsonify({
+        list: [
+            {
+                title: '默认分组',
+                tracks,
+            },
+        ],
+    })
 }
 
 async function getPlayinfo(ext) {
     ext = argsify(ext)
-    var plays = []
-    let url = ext.url
-    
-    // 发送请求
-    const { data } = await $fetch.get(url, {
+    const { data, name } = ext
+
+    const param = data.split('').reverse().join('')
+    const url = `https://lk1.supremejav.com/supjav.php?c=${param}`
+    const res = await $fetch.get(url, {
         headers: {
             'User-Agent': UA,
-        }
-    });
-    
-    const html = data.match(/r player_.*?=(.*?)</)[1];
-    const json = JSON.parse(html);
+            Referer: `https://lk1.supremejav.com/supjav.php?l=${data}&bg=undefined`,
+        },
+    })
+    let playUrl = ''
 
-    const playurl = json.url;
-    const from = json.from;
-    if (json.encrypt == '1') {
-        playurl = decodeURIComponent(url)
-    } else if (json.encrypt == '2') {
-        playurl = decodeURIComponent(Buffer.from(url, 'base64').toString('utf-8'))
-    }
-    $print(`playurl: ${playurl}`)
-    if (playurl.indexOf('m3u8') >= 0 || playurl.indexOf('mp4') >= 0) {
-        return jsonify({ 'urls': [playurl, ] })
-    } else if (from.indexOf('line3') >= 0 || from.indexOf('line5') >= 0) {
-        const { data } = await $fetch.get(playurl, {
-            headers: {
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "zh-CN,zh;q=0.9",
-                "Referer": "https://www.zxzja.com/",
-                "Sec-Ch-Ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
-                "Sec-Ch-Ua-Mobile": "?0",
-                "Sec-Ch-Ua-Platform": "\"macOS\"",
-                "Sec-Fetch-Dest": "iframe",
-                "Sec-Fetch-Mode": "navigate",
-                "Sec-Fetch-Site": "same-site",
-                "Upgrade-Insecure-Requests": "1",
-                "User-Agent": UA,
+    if (name === 'TV') {
+        const config = res.data
+            .match(/decodeURIComponent\(escape\(r\)\)\}(.*)\)/)[1]
+            .replace(/["\(\)]/g, '')
+            .split(',')
+
+        const decrypted = decrypt(...config)
+        playUrl = decrypted.match(/var urlPlay = '(.*?)';/)[1]
+
+        function decrypt(h, u, n, t, e, r) {
+            r = ''
+            for (var i = 0, len = h.length; i < len; i++) {
+                var s = ''
+                while (h[i] !== n[e]) {
+                    s += h[i]
+                    i++
+                }
+                for (var j = 0; j < n.length; j++) s = s.replace(new RegExp(n[j], 'g'), j)
+                r += String.fromCharCode(_0xe99c(s, e, 10) - t)
             }
-        });
-        let resultv2 = data.match(/var result_v2 = {(.*?)};/)[1]
-        let code = JSON.parse('{' + resultv2 + '}').data.split('').reverse()
-        let temp = ''
-        for (let i = 0x0; i < code.length; i = i + 0x2) {
-            temp += String.fromCharCode(parseInt(code[i] + code[i + 0x1], 0x10))
+            return decodeURIComponent(escape(r))
         }
-        const purl = temp.substring(0x0, (temp.length - 0x7) / 0x2) + temp.substring((temp.length - 0x7) / 0x2 + 0x7)
-        $print('***在线之家purl =====>' + purl);
-        return jsonify({ 'urls': [purl, ] })
+
+        function _0xe99c(d, e, f) {
+            let str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'
+            var g = str.split('')
+            var h = g.slice(0, e)
+            var i = g.slice(0, f)
+            var j = d
+                .split('')
+                .reverse()
+                .reduce(function (a, b, c) {
+                    if (h.indexOf(b) !== -1) return (a += h.indexOf(b) * Math.pow(e, c))
+                }, 0)
+            var k = ''
+            while (j > 0) {
+                k = i[j % f] + k
+                j = (j - (j % f)) / f
+            }
+            return k || '0'
+        }
+    } else if (name === 'FST') {
+        const $ = cheerio.load(res.data)
+        $('script').each((_, e) => {
+            if ($(e).text().includes('eval')) {
+                const script = $(e).text().replace('eval', '')
+                const result = eval(script)
+                playUrl = result.match(/sources:\[\{file:"(.*?)"\}\]/)[1]
+            }
+        })
+    } else if (name === 'ST') {
+        const $ = cheerio.load(res.data)
+        let robot = $('#robotlink').text()
+        robot = robot.substring(0, robot.indexOf('&token=') + 7)
+        $('script').each((_, e) => {
+            let script = $(e).text()
+            if (script.includes("getElementById('robotlink')")) {
+                let token = script.split('&token=')[1].split("'")[0]
+                playUrl = 'https:/' + robot + token + '&stream=1'
+            }
+        })
+    } else if (name === 'VOE') {
+        const location = res.data.match(/window\.location\.href = '(.*?)';/)[1]
+        const locres = await $fetch.get(location, {
+            headers: {
+                'User-Agent': UA,
+            },
+        })
+        playUrl = locres.data.match(/prompt\("Node", "(.*?)"\);/)[1]
     }
-    return jsonify({ 'urls': [] })
+
+    return jsonify({ urls: [playUrl] })
 }
 
 async function search(ext) {
-  ext = argsify(ext)
-  var cards = [];
+    ext = argsify(ext)
+    let cards = []
 
-  let text = encodeURIComponent(ext.text)
-  let page = ext.page || 1
-  
-  if (page > 1) {
-    return jsonify({
-      list: cards,
-    });
-  }
+    let text = encodeURIComponent(ext.text)
+    let page = ext.page || 1
+    let url = `${appConfig.site}/page/${page}?s=${text}`
 
-  const url = `https://www.zxzja.com/vodsearch/-------------.html?wd=${text}&submit=`
-  const { data } = await $fetch.get(url, {
-    headers: {
-      'Referer': 'https://www.zxzja.com/',
-      'User-Agent': UA,
-    }
-  });
-  
-  const $ = cheerio.load(data)
-  $('a.lazyload').each((_, element) => {
-    const href = $(element).attr('href')
-    const title = $(element).attr('title')
-    const cover = $(element).attr('data-original')
-    const subTitle = $(element).find('.text-right').text()
-
-    if (href.startsWith('/detail/')) {
-      cards.push({
-        vod_id: href.replace(/.*?\/detail\/(.*).html/g, '$1'),
-        vod_name: title,
-        vod_pic: cover,
-        vod_remarks: subTitle,
-        ext: {
-          url: `${appConfig.site}${href}`,
+    const { data } = await $fetch.get(url, {
+        headers: {
+            'User-Agent': UA,
         },
-      });
-    }
-  });
+    })
+    const $ = cheerio.load(data)
 
-  return jsonify({
-      list: cards,
-  });
+    $('.post').each((_, e) => {
+        const href = $(e).find('a').attr('href')
+        const title = $(e).find('a').attr('title')
+        const cover = $(e).find('a img').attr('data-original')
+        const remarks = $(e).find('.con .meta .date').text()
+        cards.push({
+            vod_id: href,
+            vod_name: title,
+            vod_pic: cover,
+            vod_remarks: remarks,
+            ext: {
+                url: href,
+            },
+        })
+    })
 
+    return jsonify({
+        list: cards,
+    })
 }
