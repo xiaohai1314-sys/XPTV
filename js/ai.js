cafe4536a18a121517336fa6dd31c837b989edfe
xiaohai1314-sys <1083328569@qq.com>
2025-11-18 16:36:45 +0800

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -2,7 +2,7 @@
 // 描述: “趣乐兔”搜索插件 - 完整版（精准分页 + 统一海报 + 稳定兼容 + 分页锁）
 
 // ================== 配置区 ==================
-const API_ENDPOINT = "http://192.168.1.7:3005/search";
+const API_ENDPOINT = "http://192.168.10.105:3005/search";
 const SITE_URL = "https://pan.funletu.com";
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36';
 const DEBUG = true;
